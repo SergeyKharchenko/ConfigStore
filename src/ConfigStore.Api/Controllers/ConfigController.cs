994d914ac2cc6f92fc69673f17f5667fc7cacd63
sik <sik@simcorp.com>
2017-12-07 20:13:38 +0200

    Create api project

---
+++ src/ConfigStore.Api/Controllers/ConfigController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigStore.Api.Controllers {
+    [Route("api/[controller]")]
+    public class ConfigController : Controller {
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id) {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value) { }
+    }
+}
