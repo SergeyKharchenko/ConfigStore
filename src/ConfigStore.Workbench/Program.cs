f11e47fb07220c2d0c775ebdd7543e9870080d40
sik <sik@simcorp.com>
2017-12-18 22:24:05 +0200

    Add workbench

---
+++ src/ConfigStore.Workbench/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ConfigStore.Workbench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MD5Hash("sad"));
+            Console.ReadKey();
+        }
+
+        public static string MD5Hash(string input)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var result = md5.ComputeHash(Encoding.ASCII.GetBytes(input));
+                return Encoding.ASCII.GetString(result);
+            }
+        }
+    }
+}
